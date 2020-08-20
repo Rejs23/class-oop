130bf7a4219379a3d357fe9b98cde7fd1b381308
Reski Junaidi Shalat <55343378+Rejs23@users.noreply.github.com>
2020-08-20 18:33:22 +0700

    Create object.php

---
+++ Class, Object, Method, Attribute/object.php
@@ -0,0 +1,16 @@
+<?php
+
+class Hewan{
+    var $jenisPemakan;
+    var $jumlahKaki;
+
+    function makan(){
+         return "Nom nom nom";
+    }
+}
+
+//object dari class Hewan
+$singa = new Hewan();
+$kucing = new Hewan();
+
+>
